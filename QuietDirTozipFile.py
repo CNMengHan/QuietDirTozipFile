e4d87d39099726d3e618738e04f10ca9056c0f2b
XinghaiStar <xinghaiqaq@gmail.com>
2024-12-26 15:16:36 +0800

    Update QuietDirTozipFile.py

--- QuietDirTozipFile.py
+++ QuietDirTozipFile.py
@@ -14,9 +14,15 @@ def main():
     source_folder = args.source_folder
     zip_file = args.zip_file
 
+    if os.path.exists(zip_file):
+        print(f"{zip_file} already exists.")
+        return
+
     if os.path.exists(source_folder) and os.path.isdir(source_folder):
         shutil.make_archive(zip_file.replace('.zip', ''), 'zip', source_folder)
-        print(f"Finish {source_folder} > {zip_file}")
+        print(f"Finished {source_folder} > {zip_file}")
+    else:
+        print(f"{source_folder} does not exist or is not a valid directory.")
 
 
 if __name__ == "__main__":
